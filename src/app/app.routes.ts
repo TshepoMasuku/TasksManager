03e419ff6577e8fdc0632798b7cb2f6104a999bd
TshepoMasuku <tshepomasuku16@gmail.com>
2024-11-26 19:39:17 +0200

    created the required 3components && page-not-found build done!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"login", component: LoginComponent},
+  {path:"register", component: RegistrationComponent},
+  {path:"todo-list", component: TodoListComponent},
+  {path:"**",pathMatch:"full",component: PageNotFoundComponent},
+];
