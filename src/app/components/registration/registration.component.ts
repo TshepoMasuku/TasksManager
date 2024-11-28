bd63f9fc89f23c53778ee177f821b52ff7ab78f6
TshepoMasuku <tshepomasuku16@gmail.com>
2024-11-29 00:40:21 +0200

    Html & Css UI build complete!

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
+  constructor(private router:Router) {}
 
+  goToSignup() {
+    this.router.navigate(["/login"])
+  }
+  goToTodoList() {
+    this.router.navigate(["/todo-list"])
+  }
 }
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { TaskComponent } from './../task/task.component';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
+
+  constructor() {}
+
+  showCalendar() {
+    alert("PICK THE TASKS DUE DATE.(OPTIONAL)");
+  }
 
 }
